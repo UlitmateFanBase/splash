1a991099daa861fc1a88ae0619eb66ce977f8f8c
Brian Gates <core.safecrlf>
2015-11-07 21:26:35 -0500

    thank you page

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -2,7 +2,7 @@ class LandingController < ApplicationController
   def splash
   end
 
-  def landing
+  def thank_you
     @teams = Team.joins(:users).group("users.team_id").order("count(users.team_id) DESC")
   end
 end
